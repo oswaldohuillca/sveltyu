45fe6a86ba9f8f75524a90b84db09eaf95e86a63
oswaldo <oswaldo@oswaldo.oswaldo>
2021-10-31 15:07:42 -0500

    Update Alert component

--- src/components/YuAlert/YuAlert.svelte
+++ src/components/YuAlert/YuAlert.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { fly } from 'svelte/transition';
   export let color:
     | 'primary'
     | 'secondary'
@@ -7,11 +8,19 @@
     | 'danger'
     | 'black'
     | 'white' = 'primary';
+  export let open: boolean = true;
+
+  function setOpen() {
+    open = false;
+  }
 </script>
 
-<div class="alert alert-{color}">
-  <slot />
-</div>
+{#if open}
+  <div class="alert alert-{color}" transition:fly={{ y: 100, duration: 1000 }}>
+    <span class="material-icons alert-close" on:click={setOpen}> clear </span>
+    <slot />
+  </div>
+{/if}
 
 <style lang="scss">
   @import '../../scss/main.scss';
@@ -19,10 +28,30 @@
   .alert {
     border-radius: $border-radius;
     padding: 15px;
+    position: relative;
+    padding-right: 40px;
     @each $i, $color in $colors {
-      &.alert-#{$i} {
+      &.alert-#{'' + $i} {
         background: getColor($color, 0.3);
         color: $color;
+      }
+    }
+    .alert-close {
+      position: absolute;
+      top: 9px;
+      right: 9px;
+      width: 30px;
+      height: 30px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border: none;
+      border-radius: 10px;
+      cursor: pointer;
+      transition: all 0.3s ease;
+      &:hover {
+        transform: translateY(-3px);
+        box-shadow: getShadow($black);
       }
     }
   }
